b0d276c206ad41abe430ae332a0a7a5fc10ec5f1
Marek Kucmierz <marek.kucmierz@gmail.com>
2023-04-01 15:32:07 +0200

    rozwiazanie zadania #1

---
+++ 00-prework-js/main.js
@@ -0,0 +1,40 @@
+const products = [
+    {
+        name: 'banan',
+        price: 4.99
+    },
+    {
+        name: "chleb",
+        price: 3.25
+    },
+    {
+        name: 'ser',
+        price: 7.00
+    },
+    {
+        name: 'baton',
+        price: 1.99
+    }
+]
+
+//   Za pomocą JS oblicz średnią cenę produktu oraz jego sumę produktow w koszyku.
+
+// console.log('hello');
+const getSum = (array) => {
+    let sum = 0;
+    array.forEach(element => {
+        sum += element.price;
+    });
+    return sum;
+};
+
+const getAvg = (array) => {
+    let sum = 0;
+    array.forEach(element => {
+        sum += element.price;
+    });
+    return (sum / array.length).toFixed(2);
+}
+
+console.log(`Suma cen: ${getSum(products)}`);
+console.log(`Srednia cena: ${getAvg(products)}`);
