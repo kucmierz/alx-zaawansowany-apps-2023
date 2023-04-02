91aecf0e13491f82fbd07755949f21b51fa95fa4
Marek Kucmierz <marek.kucmierz@gmail.com>
2023-04-02 10:15:32 +0200

    dzien drugi

---
+++ 00-powtorka-js/js/main.js
@@ -0,0 +1,30 @@
+// Napisz funkcje o nazwie getTheOldestPerson, ktora obliczy, ktora osoba jest najstarsza w podanej tablicy obiektow. Imie osoby najstarszej, wypisz w konsoli
+
+// Gotowe rozwiazanie wrzuc na GIT i wyslij link.
+
+const people = [
+    {
+        name: "Michał",
+        age: 12
+    },
+    {
+        name: "Damian",
+        age: 10
+    },
+    {
+        name: "Wiktoria",
+        age: 14
+    },
+    {
+        name: "Agata",
+        age: 8
+    }
+]
+
+// Final Output:
+
+// Wiktoria
+
+const getTheOldestPerson = (array) => {
+
+};
