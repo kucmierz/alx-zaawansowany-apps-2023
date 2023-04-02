1146fc133c85f8654c305e43e46ee942661abbcb
Marek Kucmierz <marek.kucmierz@gmail.com>
2023-04-02 16:28:34 +0200

    tworzymy slider do zdjec

---
+++ 02-dom-slider/js/main.js
@@ -0,0 +1,11 @@
+// Napisz kod slidera
+
+// 1. Stworz sobie pusta tablice images
+// 2. Do tablicy images dodaj adresy zdjec pieskow z internetu
+//   -> Hint: aby wziac zdjecie pieska z internetu, wejdz na google images, kliknij prawy przycisk i wybierz Copy Image Adress
+// 3. Dodaj zmienna counter i ustaw jej wartosc poczatkowa na 0
+// 4. po zaladowaniu pliku JS, dodaj elementowi img atrybut src, odpowiadajacy pierwszemu elementowi tablicy images, zdefiniowanym w kroku 1
+// 5. po wcisnieciu przycisku next, zastap atrybut src obrazka nastepnym elementem z tablicy
+// 6. po wcisnieciu przycisku prev, zastap atrybut src obrazka poprzednim elementem z tablicy
+// 7. po wcisnieciu przycisku next, w momencie kiedy jest ostatni element slidera, wstaw 1 zdjecie
+// 8. po wcisnieciu przycisku prev, w momencie kiedy jest pierwszy element slidera, wstaw ostatnie zdjecie
