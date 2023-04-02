6f1554b76c4690637a850c15372122598ded2756
Marek Kucmierz <marek.kucmierz@gmail.com>
2023-04-02 14:32:42 +0200

    rozwiazanie zadan DOM

---
+++ 01-dom-podstawy/js/main.js
@@ -0,0 +1,61 @@
+
+// Za pomoca kodu JS wykonaj następujące zadania.
+// Pamietaj, aby elementom HTML dodawac ID.
+
+// 1. W konsoli wypisz obecny tekst przycisku
+const btnSubmit = document.querySelector('#btnSubmit');
+console.log(btnSubmit.innerText);
+
+// 2. dodaj formularzowi klase my-form
+const form = document.querySelector('form');
+form.classList.add('my-form');
+
+// 3. Za pomoca JS dodaj do inputow jakis tekst
+const inputName = document.querySelector('#inputName');
+const inputMessage = document.querySelector('#inputMessage');
+inputName.value = 'Helmut';
+inputMessage.value = 'Musze jutro pojsc do pracy';
+
+// 4. do elementu ul dodaj element li
+{/* <li>
+      <strong>Ada</strong> napisala
+      <p> Super dzien. Duzo sie nauczylam </p>
+    </li> */}
+
+const chatList = document.querySelector('#chatList');
+chatList.innerHTML += `
+    <li>
+      <strong>Ada</strong> napisala
+      <p> Super dzien. Duzo sie nauczylam </p>
+    </li>
+`;
+
+// 5.  Majac tablice obiektow messages, za pomoca petli dodaj do ul kilka elementow li
+
+const messages = [
+    {
+        author: 'Pawel',
+        message: 'Ale dzisiaj super dzien'
+    },
+    {
+        author: "Magda",
+        message: "Zimno jest"
+    }
+];
+
+const newMessage = (author, message, htmlList) => {
+    htmlList.innerHTML += `
+        <li>
+        <strong>${author}</strong> napisal(a)
+        <p> ${message} </p>
+        </li>
+    `;
+};
+
+const addMessages = (messagesArray) => {
+    for (const message of messagesArray) {
+        newMessage(message.author, message.message, chatList);
+    }
+};
+
+addMessages(messages);
