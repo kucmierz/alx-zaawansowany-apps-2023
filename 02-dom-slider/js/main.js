c2f14485206dbe176bc5afce1b0d122b1a1c6848
Marek Kucmierz <marek.kucmierz@gmail.com>
2023-04-02 16:46:30 +0200

    Slider rozwiazanie

--- 02-dom-slider/js/main.js
+++ 02-dom-slider/js/main.js
@@ -9,3 +9,48 @@
 // 6. po wcisnieciu przycisku prev, zastap atrybut src obrazka poprzednim elementem z tablicy
 // 7. po wcisnieciu przycisku next, w momencie kiedy jest ostatni element slidera, wstaw 1 zdjecie
 // 8. po wcisnieciu przycisku prev, w momencie kiedy jest pierwszy element slidera, wstaw ostatnie zdjecie
+
+const image = document.querySelector('img');
+const btnPrev = document.querySelector('#btnPrev');
+const btnNext = document.querySelector('#btnNext');
+
+const images = [
+    {
+        src: 'https://i1.kwejk.pl/k/obrazki/2015/05/f85b6b02bbb1dbf9e26a635e0c4e585b.jpg',
+        alt: 'piesek 1'
+    },
+    {
+        src: 'https://i.pinimg.com/736x/34/de/34/34de34a375dac96e8da6a3a1b7629c9a.jpg',
+        alt: 'piesek 2'
+    },
+    {
+        src: 'https://i1.kwejk.pl/k/obrazki/2022/04/Jm6lhjIWhASb4T52.jpg',
+        alt: 'piesek 3'
+    },
+    {
+        src: 'https://zoonews.pl/wp-content/uploads/2022/01/chihuahua.jpg',
+        alt: 'piesek 4'
+    }
+];
+
+let counter = 0;
+image.src = images[0].src;
+
+const handleNextButton = (ev) => {
+    ev.preventDefault();
+    if (counter + 1 >= images.length) {
+        counter = -1;
+    }
+    image.src = images[++counter].src;
+};
+
+const handlePrevButton = (ev) => {
+    ev.preventDefault();
+    if (counter - 1 < 0) {
+        counter = images.length;
+    }
+    image.src = images[--counter].src;
+};
+
+btnNext.addEventListener('click', handleNextButton);
+btnPrev.addEventListener('click', handlePrevButton);
