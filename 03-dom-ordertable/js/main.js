c36274685005828c0e493c749059d060f69936d1
Marek Kucmierz <marek.kucmierz@gmail.com>
2023-04-03 20:26:00 +0200

    zadanie domowe po pierwszym zjezdzie - poczatek

---
+++ 03-dom-ordertable/js/main.js
@@ -0,0 +1,6 @@
+// 1. Napisz kod JS, ktory dodaje elementy do tabeli
+// 2. Po wyslaniu formularza, wyczysc pola formularza
+// 3. Pod tabela dodaj przycisk usun rekordy, ktory usunie wszystkie elementy z tabeli%       
+
+
+console.log('ertzui');
