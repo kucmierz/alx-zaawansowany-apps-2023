8f56ecaeafa31000e831930150b2ce3c5aa24082
Marek Kucmierz <marek.kucmierz@gmail.com>
2023-04-03 20:57:17 +0200

    rozwiazanie zadania domowego

--- 03-dom-ordertable/js/main.js
+++ 03-dom-ordertable/js/main.js
@@ -2,5 +2,44 @@
 // 2. Po wyslaniu formularza, wyczysc pola formularza
 // 3. Pod tabela dodaj przycisk usun rekordy, ktory usunie wszystkie elementy z tabeli%       
 
+const productForm = document.querySelector('#productForm');
+const idInput = document.querySelector('#id');
+const nameInput = document.querySelector('#name');
+const priceInput = document.querySelector('#price');
+const productTable = document.querySelector('#productTable');
+const btnClear = document.querySelector('#btnClear');
 
-console.log('ertzui');
+const newProduct = (id, name, price, htmlElem) => {
+    htmlElem.innerHTML += `
+    <tr>
+        <td id="id">${id}</td>
+        <td id="name">${name}</td>
+        <td id="price">$${price}</td>
+    </tr>
+    `
+};
+
+const resetForm = () => {
+    idInput.value = '';
+    nameInput.value = '';
+    priceInput.value = '';
+};
+
+const clearHTMLElem = (htmlElem) => {
+    htmlElem.innerHTML = '';
+};
+
+const handleSubmit = (ev) => {
+    ev.preventDefault();
+
+    newProduct(idInput.value, nameInput.value, priceInput.value, productTable);
+    resetForm();
+};
+
+const handleBtnClear = (ev) => {
+    clearHTMLElem(productTable);
+};
+
+productForm.addEventListener('submit', handleSubmit);
+btnClear.addEventListener('click', handleBtnClear);
+
