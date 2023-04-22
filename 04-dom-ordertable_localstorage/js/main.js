9db7c560d1819c1346b4b45f8ef4aadec2087078
Marek Kucmierz <marek.kucmierz@gmail.com>
2023-04-22 14:50:31 +0200

    dodanie localstorage

--- 04-dom-ordertable_localstorage/js/main.js
+++ 04-dom-ordertable_localstorage/js/main.js
@@ -9,27 +9,30 @@ const calculateOrders = document.querySelector("#calculateOrders");
 const ordersSum = document.querySelector("#ordersSum");
 const ordersAvgPrice = document.querySelector("#ordersAvgPrice");
 
+let orders = JSON.parse(localStorage.getItem("orders"));
+if (orders === null) {
+  orders = []; //gdy nie a nic w localstorage, to musimy utworzyc pusta tablice, aby reszta kod sie nie wykrzaczyla
+}
 // struktura danych: tablica obiektow
 // 1. Obiekty musza miec takie same pola
 // 2. kolejnosc zamowien ma znaczenie pod katem dodania do html
-
-let orders = [
-  {
-    id: 1,
-    name: "Banan",
-    price: 9.99,
-  },
-  {
-    id: 2,
-    name: "Wisnie",
-    price: 19.99,
-  },
-  {
-    id: 3,
-    name: "Cytryny",
-    price: 3.45,
-  },
-];
+// let orders = [
+//   {
+//     id: 1,
+//     name: "Banan",
+//     price: 9.99,
+//   },
+//   {
+//     id: 2,
+//     name: "Wisnie",
+//     price: 19.99,
+//   },
+//   {
+//     id: 3,
+//     name: "Cytryny",
+//     price: 3.45,
+//   },
+// ];
 
 const newProduct = (id, name, price, htmlElem) => {
   htmlElem.innerHTML += `
@@ -96,11 +99,13 @@ const handleSubmit = (ev) => {
   };
 
   orders.push(newOrder);
+  localStorage.setItem("orders", JSON.stringify(orders));
   resetForm();
 };
 
 const handleBtnClear = (ev) => {
   orders = [];
+  localStorage.removeItem("orders");
   renderOrders();
 };
 
@@ -118,3 +123,13 @@ renderOrders();
 productForm.addEventListener("submit", handleSubmit);
 btnClear.addEventListener("click", handleBtnClear);
 calculateOrders.addEventListener("click", handleCalculate);
+
+// console.log(orders);
+// const ordersAsJSON = JSON.stringify(orders);
+// console.log(x);
+// const ordersFromJSON = JSON.parse(ordersAsJSON);
+// localStorage.getItem("klucz"); //pobieranie wartosci ze schowka
+// localStorage.setItem("klucz", "wartosc"); //pzapisanie do schowka
+// localStorage.removeItem("klucz"); //usuniecie ze schowka
+
+localStorage.setItem("orders", JSON.stringify(orders));
